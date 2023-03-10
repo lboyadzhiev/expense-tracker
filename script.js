47f187cddb0db89d585d2068127c9bc3aee1cc0b
Lachezar Boyadzhiev <l.boyadzhievv@gmail.com>
2023-03-10 16:31:12 +0200

    transfer money to another account

--- script.js
+++ script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-    owner: 'Jonas Schmedtmann',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, //
-    pin: 1111,
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, //
+  pin: 1111,
 };
 
 const account2 = {
-    owner: 'Jessica Davis',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 };
 
 const account3 = {
-    owner: 'Steven Thomas Williams',
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
 };
 
 const account4 = {
-    owner: 'Sarah Smith',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -62,88 +62,126 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const dispayMovements = (movements) => {
-    containerMovements.innerHTML = '';
+  containerMovements.innerHTML = '';
 
-    movements.forEach((mov, i) => {
-        const type = mov > 0 ? 'deposit' : 'withdrawal';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-        const html = `
+    const html = `
          <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
           <div class="movements__date">3 days ago</div>
           <div class="movements__value">${mov} EUR</div>
         </div>
         `;
 
-        containerMovements.insertAdjacentHTML('afterbegin', html);
-    });
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
 };
 
-const calcDisplayBalance = (movements) => {
-    const balance = movements.reduce((acc, mov) => acc + mov, 0);
-    labelBalance.textContent = `${balance} EUR`;
+const calcDisplayBalance = (acc) => {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance} EUR`;
 };
 
 const calcDisplaySummary = (acc) => {
-    const incomes = acc.movements.filter((mov) => mov > 0).reduce((acc, mov) => acc + mov, 0);
+  const incomes = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
 
-    const outcomes = acc.movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0);
+  const outcomes = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
 
-    const interest = acc.movements
-        .filter((mov) => mov > 0)
-        .map((deposit) => (deposit * acc.interestRate) / 100)
-        .filter((int, i, arr) => {
-            console.log(arr);
-            return int >= 1;
-        })
-        .reduce((acc, curr) => acc + curr, 0);
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, curr) => acc + curr, 0);
 
-    labelSumIn.textContent = `${incomes} eur`;
-    labelSumOut.textContent = `${Math.abs(outcomes)} eur`;
-    labelSumInterest.textContent = `${interest} eur`;
+  labelSumIn.textContent = `${incomes} eur`;
+  labelSumOut.textContent = `${Math.abs(outcomes)} eur`;
+  labelSumInterest.textContent = `${interest} eur`;
 };
 
 const user = 'Stefen Thomas Williams';
 
 const createUserNames = (accounts) => {
-    accounts.forEach((acc) => {
-        acc.username = acc.owner
-            .toLocaleLowerCase()
-            .split(' ')
-            .map((name) => name[0])
-            .join('');
-    });
+  accounts.forEach((acc) => {
+    acc.username = acc.owner
+      .toLocaleLowerCase()
+      .split(' ')
+      .map((name) => name[0])
+      .join('');
+  });
 };
 
 createUserNames(accounts);
+
+const updateUi = (currentAccount) => {
+  // display movements
+  dispayMovements(currentAccount.movements);
+
+  // display balance
+  calcDisplayBalance(currentAccount);
+
+  // display summary
+  calcDisplaySummary(currentAccount);
+};
 
 // EVENT HANDLERS LOGIN
 let currentAccount;
 
 btnLogin.addEventListener('click', (e) => {
-    e.preventDefault();
-    currentAccount = accounts.find((acc) => acc.username === inputLoginUsername.value);
+  e.preventDefault();
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
 
-    if (currentAccount?.pin === Number(inputLoginPin.value)) {
-        // display UI and welcome message
-        labelWelcome.textContent = `Welcome back ${currentAccount.owner.split(' ')[0]}`;
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // display UI and welcome message
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
 
-        containerApp.style.opacity = 100;
+    containerApp.style.opacity = 100;
 
-        inputLoginUsername.value = '';
-        inputLoginPin.value = '';
+    inputLoginUsername.value = '';
+    inputLoginPin.value = '';
 
-        inputLoginPin.blur();
+    inputLoginPin.blur();
 
-        // display movements
-        dispayMovements(currentAccount.movements);
+    updateUi(currentAccount);
+  }
+});
 
-        // display balance
-        calcDisplayBalance(currentAccount.movements);
+// EVENT HANDLERS TRANSFER MONEY
+btnTransfer.addEventListener('click', (e) => {
+  e.preventDefault();
 
-        // display summary
-        calcDisplaySummary(currentAccount);
-    }
+  const amount = Number(inputTransferAmount.value);
+  const receiverAccaount = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+
+  if (
+    amount > 0 &&
+    receiverAccaount &&
+    currentAccount.balance >= amount &&
+    receiverAccaount?.username !== currentAccount.username
+  ) {
+    currentAccount.movements.push(-amount);
+    receiverAccaount.movements.push(amount);
+
+    updateUi(currentAccount);
+  }
+
+  inputTransferAmount.value = inputTransferTo.value = '';
 });
 
 /////////////////////////////////////////////////
@@ -151,9 +189,9 @@ btnLogin.addEventListener('click', (e) => {
 // LECTURES
 
 const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
